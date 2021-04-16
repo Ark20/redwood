544d8c7675882bef8730d20a7e7cd870c9e7978e
ark20 <regwill909@gmail.com>
2021-04-16 13:30:39 -0700

    complete

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,24 +1,35 @@
+// web/src/layouts/BlogLayout/BlogLayout.js
+
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
-  return  <>
-    <header>
-    <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-    <nav>
-      <ul>
-        <li>
-          <Link to={routes.about()}>About</Link>
+  const { logIn } = useAuth()
+  console.log(useAuth.logIn);
+
+  return (
+    <div>
+      <h1>
+        <Link to={routes.home()}>Redwood Blog</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
           </li>
           <li>
             <Link to={routes.contact()}>Contact</Link>
           </li>
-      </ul>
-    </nav>
-  </header>
-  <main>{children}</main>
-</>
+          <li>
+            <button onClick={logIn}>
+              Log In
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <main>{children}</main>
+    </div>
+  )
 }
 
 export default BlogLayout
